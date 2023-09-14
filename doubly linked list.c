cb0077de2d98118370153044b9c97fd961212735
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2023-09-15 01:34:50 +0530

    Create doubly linked list.c

---
+++ doubly linked list.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node 
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+int main()
+{
+    struct node *head=NULL,*n,*temp,*last;
+    int a,count;
+    printf("Enter the number of data you want to enter : ");
+    scanf("%d",&a);
+    for (int i=0;i<a;i++)
+    {
+        count++;
+        n=(struct node *)malloc(sizeof(struct node));
+        n->next=NULL;
+        n->prev=NULL;
+        printf("Enter data : ");
+        scanf("%d",&n->data);
+        if (head == NULL)
+        {
+            head=n;
+        }
+        else{
+            temp=head;
+            while (temp->next !=NULL)
+            {
+                temp=temp->next;
+            }
+            temp->next=n;
+            n->prev=temp;
+        }
+    }
+    printf("count : %d\n",count);
+    printf("List in Ascending order!\n");
+    temp=head;
+    while (temp->next!=NULL)
+    {
+        printf("%d\t",temp->data);
+        temp=temp->next;
+
+    }
+    printf("%d",temp->data);
+    printf("\n");
+    printf("List in Descending order!\n");
+    last=temp;
+    while (last->prev!=NULL)
+    {
+        printf("%d\t",last->data);
+        last=last->prev;
+
+    }
+    printf("%d",last->data);
+    return 0;
+}
